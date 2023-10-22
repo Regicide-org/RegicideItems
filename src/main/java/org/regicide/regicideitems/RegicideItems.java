9366067941ebaa775419288847fb57bce0137608
Theorenter <tovarishodmen@gmail.com>
2023-10-22 18:31:35 +0300

    Edit .gitignore; Separate shaped and shapeless recipes loading; Now recipe config files loading from RecipeManager;

--- src/main/java/org/regicide/regicideitems/RegicideItems.java
+++ src/main/java/org/regicide/regicideitems/RegicideItems.java
@@ -1,59 +1,24 @@
 package org.regicide.regicideitems;
 
-import org.bukkit.configuration.InvalidConfigurationException;
-import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.regicide.regicideitems.entities.recipes.RecipeManager;
 
-import java.io.File;
-import java.io.IOException;
-
 public final class RegicideItems extends JavaPlugin {
     private static RegicideItems instance;
-
-    private File customConfigFile;
-    private FileConfiguration craftRecipeConfig;
 
     public static RegicideItems getInstance() {
         return instance;
     }
 
-    public FileConfiguration getCraftRecipeConfig() {
-        return craftRecipeConfig;
-    }
-
-    private void createCustomConfig() {
-        customConfigFile = new File(getDataFolder(),
-                "items" + File.separator + "recipes" + File.separator + "craft-recipes.yml");
-        if (!customConfigFile.exists()) {
-            customConfigFile.getParentFile().mkdirs();
-            saveResource("items" + File.separator +
-                    "recipes" + File.separator + "craft-recipes.yml", false);
-        }
-        craftRecipeConfig = new YamlConfiguration();
-        try {
-            craftRecipeConfig.load(customConfigFile);
-        } catch (IOException | InvalidConfigurationException e) {
-            getLogger().severe(e.getMessage());
-        }
-    }
-
-
     @Override
     public void onEnable() {
         // Plugin startup logic
         instance = this;
-        createCustomConfig();
-        RecipeManager.readCraftRecipes();
 
-
-
-
-
-
-
-
+        getLogger().info("Loading custom recipes...");
+        RecipeManager.readShapedRecipes();
+        RecipeManager.readShapelessRecipes();
+        getLogger().info("All custom recipes was successfully loaded!");
     }
 
     @Override
--- src/main/java/org/regicide/regicideitems/entities/recipes/RecipeManager.java
+++ src/main/java/org/regicide/regicideitems/entities/recipes/RecipeManager.java
@@ -4,58 +4,95 @@ import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
 import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.configuration.InvalidConfigurationException;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.ShapedRecipe;
-import org.checkerframework.checker.nullness.qual.NonNull;
+import org.jetbrains.annotations.NotNull;
 import org.regicide.regicideitems.RegicideItems;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
-import java.util.Map;
-import java.util.Objects;
 import java.util.Set;
 
+/**
+ * This class responsible for loading custom recipes.
+ */
 public final class RecipeManager {
-    public static void readCraftRecipes() {
+    /**
+     * Loads shaped recipes from plugin data folder ./shaped-recipes.yml.
+     */
+    @SuppressWarnings({"ConstantConditions", "unchecked"})
+    public static void readShapedRecipes() {
+        FileConfiguration shapedRecipesConfig =
+                getRecipesConfiguration("items" + File.separator + "recipes" + File.separator + "shaped-recipes.yml");
+        ConfigurationSection shapedRecipes = shapedRecipesConfig.getConfigurationSection("shaped-recipes");
 
-        ConfigurationSection cSection = RegicideItems.getInstance().getCraftRecipeConfig().getConfigurationSection("craft-recipes");
-        Set<String> recipes = cSection.getKeys(false);
+        Set<String> recipes = shapedRecipes.getKeys(false);
         for (String recipe : recipes) {
-            String materialName = cSection.getString(recipe + ".result.item");
-            int amount = cSection.getInt(recipe + ".result.amount");
-            boolean shapeBool = cSection.getBoolean(recipe + ".shape.shaped");
-            List<List<String>> shapes = (List<List<String>>) cSection.get(recipe + ".shape.shape-patterns");
 
-            ItemStack item = new ItemStack(Material.getMaterial(materialName), amount);
+            // Fields of recipe
+            String materialName = shapedRecipes.getString(recipe + ".result.item");
+            int amount = shapedRecipes.getInt(recipe + ".result.amount");
+            List<List<String>> shapes = (List<List<String>>) shapedRecipes.get(recipe + ".shape.shape-patterns");
+            ItemStack resultItem = new ItemStack(Material.getMaterial(materialName), amount);
+
 
             int i = 0;
-            if (shapeBool) {
-                for (List<String> shape : shapes) {
-                    String s1 = shape.get(0);
-                    String s2 = shape.get(1);
-                    String s3 = shape.get(2);
-                    ShapedRecipe craftRecipe = new ShapedRecipe(new NamespacedKey(RegicideItems.getInstance(),
-                            recipe + i), item);
-                    i++;
-                    craftRecipe.shape(s1, s2, s3);
+            for (List<String> shape : shapes) {
 
-                    ConfigurationSection cSectionKeys = cSection.getConfigurationSection(recipe + ".shape.keys");
-                    Set<String> keys = cSectionKeys.getKeys(false);
-                    for (String key : keys) {
-                        char chrKey = key.charAt(0);
-                        craftRecipe.setIngredient(chrKey, Material.getMaterial(cSectionKeys.getString("." + key + ".item")));
+                String s1 = shape.get(0);
+                String s2 = shape.get(1);
+                String s3 = shape.get(2);
 
-                    }
+                ShapedRecipe craftRecipe = new ShapedRecipe(new NamespacedKey(RegicideItems.getInstance(),
+                        recipe + i), resultItem);
+                i++;
+                craftRecipe.shape(s1, s2, s3);
 
-                    Bukkit.addRecipe(craftRecipe);
+                ConfigurationSection cSectionKeys = shapedRecipes.getConfigurationSection(recipe + ".shape.keys");
+                assert cSectionKeys != null;
+                Set<String> keys = cSectionKeys.getKeys(false);
+                for (String key : keys) {
+                    char chrKey = key.charAt(0);
+                    craftRecipe.setIngredient(chrKey, Material.getMaterial(cSectionKeys.getString("." + key + ".item")));
+
                 }
 
+                Bukkit.addRecipe(craftRecipe);
             }
         }
+    }
+    /**
+     * Loads shapeless recipes from plugin data folder ./shapeless-recipes.yml.
+     */
+    public static void readShapelessRecipes() {
+        FileConfiguration shapelessRecipesConfig =
+                getRecipesConfiguration("items" + File.separator + "recipes" + File.separator + "shapeless-recipes.yml");
+        // TODO. Надо расписать логику реализации чтения shapelessRecipes
+    }
 
+    /**
+     * @param path The path to craft recipes.
+     * @return The file configuration.
+     */
+    @SuppressWarnings("all")
+    private static FileConfiguration getRecipesConfiguration(@NotNull final String path) {
+        File customConfigFile = new File(RegicideItems.getInstance().getDataFolder(), path);
+        if (!customConfigFile.exists()) {
+            customConfigFile.getParentFile().mkdirs();
+            RegicideItems.getInstance().saveResource(path, false);
+        }
 
-
-
+        FileConfiguration recipesConfig = new YamlConfiguration();
+        try {
+            recipesConfig.load(customConfigFile);
+        } catch (IOException | InvalidConfigurationException e) {
+            RegicideItems.getInstance().getLogger().severe(e.getMessage());
+        }
+        return recipesConfig;
     }
 
 }
