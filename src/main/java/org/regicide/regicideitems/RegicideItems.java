3d147ec26803cfba66974a6260510ae443d436b0
Pashaken <pasha.sol788@yandex.ru>
2023-11-27 23:49:31 +0300

    Deleted part of reading .yml files(add in separate class)

--- src/main/java/org/regicide/regicideitems/RegicideItems.java
+++ src/main/java/org/regicide/regicideitems/RegicideItems.java
@@ -1,6 +1,10 @@
 package org.regicide.regicideitems;
 
+import org.bukkit.Bukkit;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.regicide.regicideitems.entities.items.CustomItemManager;
 import org.regicide.regicideitems.entities.recipes.RecipeManager;
 
 public final class RegicideItems extends JavaPlugin {
@@ -18,6 +22,7 @@ public final class RegicideItems extends JavaPlugin {
         getLogger().info("Loading custom recipes...");
         RecipeManager.readShapedRecipes();
         RecipeManager.readShapelessRecipes();
+        Bukkit.getServer().getPluginManager().registerEvents(new CustomItemManager(),this);
         getLogger().info("All custom recipes was successfully loaded!");
     }
 
--- src/main/java/org/regicide/regicideitems/entities/items/CustomItemManager.java
+++ src/main/java/org/regicide/regicideitems/entities/items/CustomItemManager.java
@@ -1,6 +1,9 @@
 package org.regicide.regicideitems.entities.items;
 
+import net.kyori.adventure.text.Component;
 import org.bukkit.Material;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -10,9 +13,12 @@ import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.regicide.regicideitems.RegicideItems;
+import org.regicide.regicideitems.config.ConfigManager;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.awt.*;
+import java.io.File;
+import java.util.*;
+import java.util.List;
 
 public final class CustomItemManager implements Listener {
     private final static Map<String,ItemStack> CUSTOM_ITEM_MAP = new HashMap<>();
@@ -21,14 +27,29 @@ public final class CustomItemManager implements Listener {
      */
     @EventHandler
     public static void readCustomItems(PlayerJoinEvent event) {
-        ItemStack item =new ItemStack(Material.STICK);
-        ItemMeta meta = item.getItemMeta();
-        meta.setDisplayName("Test Item");
-        meta.getPersistentDataContainer();
-        item.setItemMeta(meta);
-        //CUSTOM_ITEM_MAP.put(id,item);
-        event.getPlayer().getInventory().setItem(2,item);
+        FileConfiguration customConfigItems =
+                ConfigManager.getConfigFromFile("items" + File.separator + "custom-items.yml");
+        ConfigurationSection customItems =  customConfigItems.getConfigurationSection("custom-items");
+
+        int i = 1;
+        Set<String> items = customItems.getKeys(false);
+        for (String item : items) {
+
+            //Fields of item
+            String materialItemName = customItems.getString(item+".type");
+            String nmitem = customItems.getString(item+".name");
+            List<Component> loreitem = new ArrayList<>();
+            for (String str : customItems.getStringList(item+".lore"))
+                loreitem.add(Component.text(str).toBuilder().build());
+
+            ItemStack newitem = new ItemStack(Material.getMaterial(materialItemName));
+            ItemMeta meta = newitem.getItemMeta();
+            meta.displayName(Component.text(nmitem).toBuilder().build());
+            meta.lore(loreitem);
+            newitem.setItemMeta(meta);
+            CUSTOM_ITEM_MAP.put(item,newitem);
+            event.getPlayer().getInventory().setItem(i,newitem);
+            i++;
+        }
     }
-
-
 }
